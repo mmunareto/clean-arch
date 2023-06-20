041caa364c022984d73d667fe3ea7c8999bb9664
mmunareto <matheus.munareto@live.com>
2023-06-20 06:20:30 -0300

    Added web server and Viper config

---
+++ cmd/core/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "github.com/streadway/amqp"
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/mmunareto/clean-arch/configs"
+	"github.com/mmunareto/clean-arch/internal/infra/event/handler"
+	"github.com/mmunareto/clean-arch/internal/infra/web/webserver"
+	"github.com/mmunareto/clean-arch/pkg/events"
+	// mysql
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rabbitMQChannel := getRabbitMQChannel()
+
+	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("OrderCreated", &handler.OrderCreatedHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+
+	webserver := webserver.NewWebServer(configs.WebServerPort)
+	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
+	webserver.AddHandler("/order", webOrderHandler.Create)
+	fmt.Println("Starting web server on port", configs.WebServerPort)
+	webserver.Start()
+}
+
+func getRabbitMQChannel() *amqp.Channel {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	return ch
+}
