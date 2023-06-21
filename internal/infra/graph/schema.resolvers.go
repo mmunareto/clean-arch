b494536f6635408c527fbd5b25d1de6319378375
mmunareto <matheus.munareto@live.com>
2023-06-21 07:11:23 -0300

    Added list orders to graphql server

--- internal/infra/graph/schema.resolvers.go
+++ internal/infra/graph/schema.resolvers.go
@@ -6,9 +6,8 @@ package graph
 
 import (
 	"context"
-	"github.com/mmunareto/clean-arch/internal/usecase"
-
 	"github.com/mmunareto/clean-arch/internal/infra/graph/model"
+	"github.com/mmunareto/clean-arch/internal/usecase"
 )
 
 // CreateOrder is the resolver for the createOrder field.
@@ -24,13 +23,35 @@ func (r *mutationResolver) CreateOrder(ctx context.Context, input *model.OrderIn
 	}
 	return &model.Order{
 		ID:         output.ID,
-		Price:      float64(output.Price),
-		Tax:        float64(output.Tax),
-		FinalPrice: float64(output.FinalPrice),
+		Price:      output.Price,
+		Tax:        output.Tax,
+		FinalPrice: output.FinalPrice,
 	}, nil
+}
+
+// Orders is the resolver for the orders field.
+func (r *queryResolver) Orders(ctx context.Context) ([]*model.Order, error) {
+	ordersFound, err := r.ListOrdersUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+	var orders []*model.Order
+	for _, o := range ordersFound {
+		orders = append(orders, &model.Order{
+			ID:         o.ID,
+			Price:      o.Price,
+			Tax:        o.Tax,
+			FinalPrice: o.FinalPrice,
+		})
+	}
+	return orders, nil
 }
 
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 
+// Query returns QueryResolver implementation.
+func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+
 type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
